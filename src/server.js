21502ce5bcc283262a0bd927edd86d61624814e2
Asaromi <a.sabililromi@gmail.com>
2023-10-14 17:24:18 +0700

    fix: add testing global api

--- src/server.js
+++ src/server.js
@@ -3,8 +3,11 @@ const express = require('express')
 
 const {PORT = 3000, HOST = 'localhost'} = process.env
 const app = express()
-app.use(express.json())
 
+app.use(express.json())
+app.get('/', (req, res) => {
+  return res.status(200).json({ status: 'OK', success: true, message: 'Welcome to global api service' })
+})
 app.use('/api', require('./routers'))
 
 app.listen(PORT, HOST, () => {
