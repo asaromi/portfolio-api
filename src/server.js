7b49525193070c784cd9e98c3edf23bd50168eba
Asaromi <a.sabililromi@gmail.com>
2023-11-01 09:38:05 +0700

    fix: new main server file

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 
 const {PORT = 3000, HOST = 'localhost'} = process.env
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
 app.get('/', (req, res) => {
   return res.status(200).json({ status: 'OK', success: true, message: 'Welcome to global api service' })
 })
+
 app.use('/api', require('./routers'))
 
 app.listen(PORT, HOST, () => {
